df93bd58e76ae43bfe94e7582d3b3d8385e6a93c
mostafamdy <mostafa_20180593@fci.helwan.edu.eg>
2024-11-21 21:33:52 +0200

    fix_message

--- Admin-Bot/src/api.ts
+++ Admin-Bot/src/api.ts
@@ -3,7 +3,7 @@ const BASE_URL = 'http://127.0.0.1:8000';
 export default {
   async getCount() {
     try {
-      const response = await fetch(`${BASE_URL}/bots/count`);
+      const response = await fetch(`${BASE_URL}/bots/count/`);
       const data = await response.json();
 
       return data;
